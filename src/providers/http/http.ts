1c27484a355977124cbe852695c9ce51f837da2d
Salvador Guzman <guzmansalv@gmail.com>
2018-06-21 04:55:22 -0700

    http provider

---
+++ src/providers/http/http.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class HttpProvider {
+  constructor(public http: HttpClient) {
+    console.log('Hello HttpProvider Provider');
+  }
+
+  public get(base: number, count: number, f: any): void {
+    this.http.get(`http://rusty-books.herokuapps.com/${base}/${count}`)
+      .subscribe((data: any) => {
+        f(data);
+      })
+  }
+}
