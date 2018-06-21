0742f6906d58c33c8bbf1b532a5b029cfee7a146
Salvador Guzman <guzmansalv@gmail.com>
2018-06-21 04:55:11 -0700

    home mod

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {HttpProvider} from "../../providers/http/http";
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers: [HttpProvider]
 })
 export class HomePage {
+  public page_idx: number = 0;
+  public page_size: number = 10;
 
-  constructor(public navCtrl: NavController) {
+  public items: any[] = [];
 
+  constructor(public http: HttpProvider) {
+    this.http.get(this.page_idx, this.page_size, data => {
+      console.log(data);
+    })
   }
 
 }
